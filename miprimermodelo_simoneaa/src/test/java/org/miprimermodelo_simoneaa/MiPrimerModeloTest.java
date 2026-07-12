4ce10b2247d7094398ba46819c158d807957a7ac
unknown <avilarranz@gmail.com>
2026-07-12 20:43:00 +0200

    feat:succesful test

--- miprimermodelo_simoneaa/src/test/java/org/miprimermodelo_simoneaa/MiPrimerModeloTest.java
+++ miprimermodelo_simoneaa/src/test/java/org/miprimermodelo_simoneaa/MiPrimerModeloTest.java
@@ -1,5 +1,24 @@
 package org.miprimermodelo_simoneaa;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Year;
+
+import org.junit.jupiter.api.Test;
+
 public class MiPrimerModeloTest {
-    
+
+    @Test
+    public void testCrearPersona() {
+        MiPrimerModelo persona = new MiPrimerModelo("Juan", "Pérez", "1234", 2000);
+
+        assertEquals("Juan", persona.getNombre());
+        assertEquals("Pérez", persona.getApellido());
+        assertEquals("1234", persona.getDocumentoIdentidad());
+        assertEquals(2000, persona.getAñoNacimiento());
+
+        int edadEsperada = Year.now().getValue() - 2000;
+        assertEquals(edadEsperada, persona.getEdad());
+    }
+
 }
