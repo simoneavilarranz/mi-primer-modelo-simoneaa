d68ad85399c098f77af20585e0df4c3ecd1c98a4
unknown <avilarranz@gmail.com>
2026-07-12 19:21:57 +0200

    feat:created miprimermodelo class

--- miprimermodelo_simoneaa/src/main/java/org/miprimermodelo_simoneaa/MiPrimerModelo.java
+++ miprimermodelo_simoneaa/src/main/java/org/miprimermodelo_simoneaa/MiPrimerModelo.java
@@ -1,5 +1,42 @@
 package org.miprimermodelo_simoneaa;
 
-public class MiPrimerModelo {
+import java.time.Year;
 
+
+public class MiPrimerModelo {
+    
+    private String nombre;
+    private String apellido;
+    private String documentoIdentidad;
+    private int añoNacimiento;
+    private int edad;
+    
+    public MiPrimerModelo(String nombre, String apellido, String documentoIdentidad, int añoNacimiento) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.documentoIdentidad = documentoIdentidad;
+    this.añoNacimiento = añoNacimiento;
+    this.edad = calcularEdad();
+    }
+    
+    private int calcularEdad() {
+        int anioActual = Year.now().getValue();
+        return anioActual - this.añoNacimiento;
+    }
+    
+    public String getNombre() {
+    return this.nombre;
+    }
+    public String getApellido() {
+    return this.apellido;
+    }
+    public String getDocumentoIdentidad() {
+    return this.documentoIdentidad;
+    }
+    public int getAñoNacimiento() {
+    return this.añoNacimiento;
+    }
+    public int getEdad() {
+    return this.edad;
+    }
 }
